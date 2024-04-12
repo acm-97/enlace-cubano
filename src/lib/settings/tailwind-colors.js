10ea8c85123445cfc1f794626d08a922103347ac
Alejandro Cabrera <alejandro97.acm@gmail.com>
2024-04-11 23:48:54 -0400

    fix tailwind config

--- src/lib/settings/tailwind-colors.js
+++ src/lib/settings/tailwind-colors.js
@@ -8,10 +8,11 @@ export function getColors() {
 
   Object.keys(colors).forEach(key => {
     const processedKey = processText(key)[0]
-    const {red, green, blue} = hexRgb(colors[key])
+    // const {red, green, blue} = hexRgb(colors[key])
     colorsRGB[processedKey[0]] = {
       ...colorsRGB[processedKey[0]],
-      [processedKey[1]]: `rgb(${red} ${green} ${blue} / <alpha-value>)`,
+      // [processedKey[1]]: `rgb(${red} ${green} ${blue} / <alpha-value>)`,
+      [processedKey[1]]: colors[key],
     }
   })
   return colorsRGB

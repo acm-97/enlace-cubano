2d35cfef33d082a1a159f98fce6b7e14d43285d2
Alejandro Cabrera <alejandro97.acm@gmail.com>
2024-04-12 00:55:13 -0400

    update top tabs

--- src/modules/recharges/components/screen.js
+++ src/modules/recharges/components/screen.js
@@ -3,6 +3,12 @@ import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs
 import NautaScreen from './nauta/screen'
 import MovilScreen from './movil/screen'
 import {config} from '@gluestack-ui/config'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+
+const menu = [
+  {label: 'Movil', icon: 'smartphone', Component: MovilScreen},
+  {label: 'Nauta', icon: 'router', Component: NautaScreen},
+]
 
 const Tab = createMaterialTopTabNavigator()
 
@@ -10,25 +16,26 @@ function RechargesScreen() {
   return (
     <Tab.Navigator
       screenOptions={{
+        tabBarItemStyle: {flexDirection: 'row', alignItems: 'center'},
         tabBarLabelStyle: {fontSize: 14, fontWeight: 600},
         tabBarActiveTintColor: config.tokens.colors.orange500,
-        tabBarIndicatorStyle: { backgroundColor: config.tokens.colors.orange500 },
+        tabBarIndicatorStyle: {backgroundColor: config.tokens.colors.orange500},
+        tabBarPressColor: config.tokens.colors.orange50,
+        tabBarPressOpacity: 0.5,
       }}
     >
-      <Tab.Screen
-        name="Movil"
-        component={MovilScreen}
-        options={{
-          swipeEnabled: false,
-        }}
-      />
-      <Tab.Screen
-        name="Nauta"
-        component={NautaScreen}
-        options={{
-          swipeEnabled: false,
-        }}
-      />
+      {menu.map(({label, icon, Component}) => (
+        <Tab.Screen
+          key={label}
+          name={label}
+          component={Component}
+          options={{
+            swipeEnabled: false,
+            tabBarLabel: label,
+            tabBarIcon: ({color, size}) => <MaterialIcons name={icon} color={color} size={20} />,
+          }}
+        />
+      ))}
     </Tab.Navigator>
   )
 }
